39cbfc9f3aa16141f0dbd8ff04f409e659c62bb9
Gustavo Raulino da Silva <gustavoraulinodasilva@gmail.com>
2025-05-12 10:17:24 -0300

    footer finished

--- frontend/src/layout/Header.tsx
+++ frontend/src/layout/Header.tsx
@@ -92,7 +92,7 @@ const Header: React.FC = () => {
                         <button className="dropdown-coin__btn" onClick={toggleDropdown}>
                             <span className="coin">
                                 <svg width="21" height="22" viewBox="0 0 21 22" fill="none" xmlns="http://www.w3.org/2000/svg">
-                                    <g clip-path="url(#clip0_1199_518)">
+                                    <g clipPath="url(#clip0_1199_518)">
                                         <path d="M11 19C15.4183 19 19 15.4183 19 11C19 6.58172 15.4183 3 11 3C6.58172 3 3 6.58172 3 11C3 15.4183 6.58172 19 11 19Z" fill="#D80027"/>
                                         <path d="M11 7.17392L11.8634 9.83108H14.6573L12.397 11.4733L13.2603 14.1305L11 12.4882L8.73971 14.1305L9.60309 11.4733L7.34277 9.83108H10.1366L11 7.17392Z" fill="#FFDA44"/>
                                     </g>
@@ -114,7 +114,7 @@ const Header: React.FC = () => {
                             <ul className="dropdown-coin__list">
                                 <li className="dropdown-coin__item">
                                     <svg width="21" height="22" viewBox="0 0 21 22" fill="none" xmlns="http://www.w3.org/2000/svg">
-                                    <g clip-path="url(#clip0_1199_518)">
+                                    <g clipPath="url(#clip0_1199_518)">
                                         <path d="M11 19C15.4183 19 19 15.4183 19 11C19 6.58172 15.4183 3 11 3C6.58172 3 3 6.58172 3 11C3 15.4183 6.58172 19 11 19Z" fill="#D80027"/>
                                         <path d="M11 7.17392L11.8634 9.83108H14.6573L12.397 11.4733L13.2603 14.1305L11 12.4882L8.73971 14.1305L9.60309 11.4733L7.34277 9.83108H10.1366L11 7.17392Z" fill="#FFDA44"/>
                                     </g>
@@ -128,7 +128,7 @@ const Header: React.FC = () => {
                                 </li>
                                 <li className="dropdown-coin__item">
                                     <svg width="21" height="22" viewBox="0 0 21 22" fill="none" xmlns="http://www.w3.org/2000/svg">
-                                    <g clip-path="url(#clip0_1199_518)">
+                                    <g clipPath="url(#clip0_1199_518)">
                                         <path d="M11 19C15.4183 19 19 15.4183 19 11C19 6.58172 15.4183 3 11 3C6.58172 3 3 6.58172 3 11C3 15.4183 6.58172 19 11 19Z" fill="#D80027"/>
                                         <path d="M11 7.17392L11.8634 9.83108H14.6573L12.397 11.4733L13.2603 14.1305L11 12.4882L8.73971 14.1305L9.60309 11.4733L7.34277 9.83108H10.1366L11 7.17392Z" fill="#FFDA44"/>
                                     </g>
@@ -142,7 +142,7 @@ const Header: React.FC = () => {
                                 </li>
                                 <li className="dropdown-coin__item">
                                     <svg width="21" height="22" viewBox="0 0 21 22" fill="none" xmlns="http://www.w3.org/2000/svg">
-                                    <g clip-path="url(#clip0_1199_518)">
+                                    <g clipPath="url(#clip0_1199_518)">
                                         <path d="M11 19C15.4183 19 19 15.4183 19 11C19 6.58172 15.4183 3 11 3C6.58172 3 3 6.58172 3 11C3 15.4183 6.58172 19 11 19Z" fill="#D80027"/>
                                         <path d="M11 7.17392L11.8634 9.83108H14.6573L12.397 11.4733L13.2603 14.1305L11 12.4882L8.73971 14.1305L9.60309 11.4733L7.34277 9.83108H10.1366L11 7.17392Z" fill="#FFDA44"/>
                                     </g>
